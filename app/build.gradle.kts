b14d13e142455a319fd07b192d0070036766b1a8
surya <suryanovenskytinus@gmail.com>
2025-09-15 16:50:23 +0700

    Commit No. 1 – Navigation drawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
 }
